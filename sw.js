6f7bd0675ec38476b391f29353d15ddb3eb4032c
Roger Kirkness <roger@convictional.com>
2018-03-18 18:11:53 -0400

    Bump version to 1.0.4

---
+++ sw.js
@@ -0,0 +1,44 @@
+---
+layout: null
+---
+
+var urls = [
+  {% for post in site.posts %}
+  '{{ post.url }}',
+  {% endfor %}
+  '/'
+]
+
+var currentCacheName = 'custom-sw-cache-' + String(urls.length) 
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+  event.waitUntil(
+    caches.open(currentCacheName).then((cache) => {
+      return cache.addAll(urls)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName.startsWith('custom-sw-cache-')
+            && cacheName != currentCacheName
+        }).map(function(cacheName){
+          return cache.delete(cacheName)
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request)
+     })
+   )
+})
